d1c30a9dc73ca44aefcb0250bf03bbfea665e529
martin <martin@comets.kr>
2024-01-09 06:14:23 +0900

    feat: configure product Schema model

--- src/train.ts
+++ src/train.ts
@@ -9,24 +9,25 @@
   
 */
 
-/*
+/* Request:
   Traditinal Api
   Rest Api
   GraphQL Api
   ...
 */
 
-
-/*
+/* Frontend Development:
   Traditional FD   =>  SSR  =>  EJS
   Modern FD        =>  SPA   =>  REACT
 */
 
-/*
+/* Cookies:
   request join
   self destroy
 */
 
-
-
-
+/* Validation:
+  Frontend validation
+  Backend validation
+  Database validation
+*/
