f2fb2a8ef7e0715c5e3a86a08326c5974d12c2a5
martin <martin@comets.kr>
2024-01-13 09:25:10 +0900

    feat: develop updateChosenUser api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -38,6 +38,15 @@ routerAdmin.post(
 );
 
 /** User */
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
+routerAdmin.get(
+  "/user/all",
+  restaurantController.verifyRestaurant,
+  restaurantController.getUsers
+);
+routerAdmin.post(
+  "/user/edit",
+  restaurantController.verifyRestaurant,
+  restaurantController.updateChosenUser
+);
 
 export default routerAdmin;
