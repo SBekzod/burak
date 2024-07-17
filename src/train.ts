cda0b7e2562bd420a503c622dd54a02529cd7eb2
martin <martin@comets.kr>
2024-07-17 19:33:33 +0900

    feat: deploy project via shell & pm2

--- src/train.ts
+++ src/train.ts
@@ -31,3 +31,17 @@
   Backend validation
   Database validation
 */
+
+/*
+ PM2 COMMANDS:
+
+ pm2 ls
+ pm2 start dist/server.js --name=BURAK
+ pm2 start "npm run start:prod" --name=BURAK
+ pm2 stop id
+ pm2 delete id
+ pm2 restart id
+ pm2 monit
+ pm2 kill
+
+*/
